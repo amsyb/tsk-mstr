a5f555530496a337e5606baa4bbcab47f661d03c
annasybingco <sybingcoanna@gmail.com>
2026-02-07 23:51:55 -0500

    added get, post and patch to server

--- server/server.js
+++ server/server.js
@@ -3,11 +3,45 @@ const cors = require("cors");
 
 const app = express();
 
+let tasks = [];
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Kanban API is running");
+});
+
+app.get("/tasks", (req, res) => {
+  res.json(tasks);
+});
+
+app.post("/tasks", (req, res) => {
+  const newTask = {
+    id: Date.now(),
+    title: req.body.title,
+    status: "todo"
+  };
+
+  tasks.push(newTask);
+
+  res.json(newTask);
+  console.log(req.body);
+
+});
+
+app.patch("/tasks/:id", (req,res) => {
+  const { id } = req.params;
+
+  const task = tasks.find(t => t.id == id);
+
+  if (!task) {
+    return res.status(404).json({ error: "Task not found" });
+  }
+
+  Object.assign(task, req.body);
+
+  res.json(task);
 });
 
 const PORT = 5050;
